22cf29f77e45d18368e5c18f6f09a33abca96236
Soumil Heble <soumil.heble@gmail.com>
2018-11-24 17:37:02 -0500

    gen_h verilog RTL, tested with 1, 7, 55 character inputs

--- project_files/HDL/run_s/gen_h.v
+++ project_files/HDL/run_s/gen_h.v
@@ -44,40 +44,26 @@ parameter width32 = 32;
 
 /** Main State Machine States */
 parameter [3:0]
-	M0 = 4'b0000,
-	M1 = 4'b0001,
-	M2 = 4'b0010,
-	M3 = 4'b0011,
-	M4 = 4'b0100,
-	M5 = 4'b0101,
-	M6 = 4'b0110,
-	M7 = 4'b0111,
-	M8 = 4'b1000,
-	M9 = 4'b1001,
-	M10 = 4'b1010,
-	M11 = 4'b1011,
-	M12 = 4'b1100,
-	M13 = 4'b1101,
-	M14 = 4'b1110,
-	M15 = 4'b1111;
-	
-/** SHA State Machine */
-parameter [2:0]
-	S0 = 3'b000,
-	S1 = 3'b001,
-	S2 = 3'b010,
-	S3 = 3'b011,
-	S4 = 3'b100,
-	S5 = 3'b101,
-	S6 = 3'b110,
-	S7 = 3'b111;
+	M0 = 4'b0000,	/** Wait for Global Main Signal */
+	M1 = 4'b0001,	/** Copy H SRAM Contents to ah Register File - Tested - Works for One Character Message - Wait for Overflow*/
+	M2 = 4'b0010,	/** Wait for W Module to be Ready */
+	M3 = 4'b0011,	/** Send Address and Enable to Fetch First K and W For SHA Iterations */
+	M4 = 4'b0100,	/** Wait For W & K to Reach Module's Input Register */
+	M5 = 4'b0101,	/** Wait For W & K to Reach Module's Input Register */
+	M6 = 4'b0110,	/** Wait For W & K to Reach Module's Input Register */
+	M7 = 4'b0111,	/** Send Address and Enable to Fetch Next K and W - Perform First Stage Operations */
+	M8 = 4'b1000,	/** Perform Second Stage Operations */
+	M9 = 4'b1001,	/** Perform Third Stage Operations */
+	M10 = 4'b1010,	/** Perform Final Stage Operations - Increment Iteration Counter and Check for Overflow */
+	M11 = 4'b1011,	/** Read H SRAM and Add To Computed AH Registers - Wait for Overflow */
+	M12 = 4'b1100,	/** Wait For Additions to Finish */
+	M13 = 4'b1101,	/** Wait For Additions to Finish */
+	M14 = 4'b1110,	/** Write AH to Output SRAM */
+	M15 = 4'b1111;	/** Finish - Wait for Next Global Main Signal */
 	
 /** State Machine Variables */
-reg [2:0] main_current_state;	/** Main State Machine State Variable */
-reg [2:0] main_next_state;		/** Main State Machine Next State */
-
-reg [2:0] sha_current_state;	/** SHA State Machine State Variable */
-reg [2:0] sha_next_state;		/** SHA State Machine Next State */
+reg [3:0] main_current_state;	/** Main State Machine State Variable */
+reg [3:0] main_next_state;		/** Main State Machine Next State */
 
 /** Registered Input Variables */
 reg regin_reset;
@@ -98,7 +84,6 @@ reg [31:0] sig1ch_add_2;	/** Add S1 and S2 Result Pipeline Register */
 reg [31:0] T2_2;			/** S6 Computation Pipeline Register */
 reg [31:0] T1_3;			/** S3 Computation Pipeline Register */
 
-
 /** Delay (Holding) Registers */
 reg [5:0] k_addr_hold0;			/** K SRAM Address Hold 0 Register */
 reg [5:0] k_addr_hold1;			/** K SRAM Address Hold 1 Register */
@@ -115,12 +100,9 @@ reg [31:0] ah_regf [0:7];	/** SHA a-h Register File */
 reg [5:0] ah_regf_addr;		/** a-h Register File Address */
 reg ah_regf_wen;			/** ah Register File Write Enable Register */
 
-/** Signals */
-reg go_iterate_sha;			/** Start SHA State Machine */
-reg done_with_sha;			/** SHA Iteration Complete */
-reg ah_access_select;		/** Control Which Data Line Gets Access to ah Register File */
-reg main_state_ah_wen;		/** Main State Machine ah Write Enable */
-reg sha_state_ah_wen;		/** SHA State Machine ah Write Enable */
+/** "Register - Signals" */
+reg finish_signal;
+reg ah_access_sig;
 
 /** Adder Module Wires */
 wire [2:0] hop_addr_sum_wire;
@@ -157,19 +139,19 @@ begin
 	if(regin_reset)
 	begin
 		/** Clear State Variables */
-		main_current_state <= 3'b0;
-		sha_current_state <= 3'b0;
+		main_current_state <= 4'b0;
 		
 		/** Important Output and Pipeline Registers */
+		/** Enables Can Mess With SRAM's and Register File Hence Deassert */
 		k_en_hold0 <= 1'b0;
 		k_en_hold1 <= 1'b0;
 		regop_k_mem_en <= 1'b0;
 		regop_w_mem_en <= 1'b0;
 		regop_h_mem_en <= 1'b0;
 		regop_op_mem_en <= 1'b0;
-		regop_finish <= 1'b0;
 		ah_regf_wen <= 1'b0;
 		ah_regf_wen_hold0 <= 1'b0;
+		regop_finish <= 1'b0;
 		
 		/** Address Counters */
 		curr_addr_hop <= 3'b0;
@@ -180,36 +162,28 @@ begin
 	begin
 		/** Take FSM To Next State */
 		main_current_state <= main_next_state;
-		sha_current_state <= sha_next_state;
 	
 		/** Feed Holding Registers */
 		k_en_hold1 <= k_en_hold0;
 		regop_k_mem_en <= k_en_hold1;
 		
-		k_addr_hold1 <= k_addr_hold0;
-		regop_k_mem_addr <= k_addr_hold1;
-		
 		ah_regf_wen <= ah_regf_wen_hold0;
 		ah_regf_addr <= ah_regf_addr_hold0;
 		
-		k_addr_hold0 <= curr_addr_kw;
+		regop_finish <= finish_signal;
+		
 		regop_w_mem_addr <= curr_addr_kw;
+		k_addr_hold0 <= curr_addr_kw;
+		k_addr_hold1 <= k_addr_hold0;
+		regop_k_mem_addr <= k_addr_hold1;
 		
-		regop_w_mem_en <= 1'b0;
-		k_en_hold0 <= 1'b0;
-		
-		if(ah_access_select)
-		begin
-			ah_regf_wen_hold0 <= sha_state_ah_wen;
-		end
-		else
-		begin
-			ah_regf_wen_hold0 <= main_state_ah_wen;
-		end
+		regop_h_mem_addr <= curr_addr_hop;
+		ah_regf_addr_hold0 <= curr_addr_hop;
+		regop_op_mem_addr <= curr_addr_hop;
 		
 		if(ah_regf_wen)
 		begin
-			if(ah_access_select)
+			if(ah_access_sig)
 			begin
 				ah_regf[ah_regf_addr] <= ah_addr_sum_wire;
 			end
@@ -218,82 +192,85 @@ begin
 				ah_regf[ah_regf_addr] <= regin_h_data_in;
 			end
 		end
+			
 		
-		if(main_current_state==M1|sha_current_state==S5)
-		begin
-			curr_addr_hop <= hop_addr_sum_wire;
-			regop_h_mem_addr <= curr_addr_hop;
-			regop_h_mem_en <= 1'b1;
-			ah_regf_addr_hold0 <= curr_addr_hop;
-		end
-		else
-		if((main_current_state==M3)|(sha_current_state==S1))
-		begin
-			curr_addr_kw <= kw_addr_sum_wire;
-			regop_w_mem_en <= 1'b1;
-			k_en_hold0 <= 1'b1;
-		end
-		else
-		if(main_current_state==M7)
-		begin
-			curr_addr_hop <= hop_addr_sum_wire;
-			regop_op_mem_addr <= curr_addr_hop;
-			regop_op_mem_en <= 1'b1;
-			regop_op_mem_wdata <= ah_regf[curr_addr_hop];
-		end
-		else
-		if(main_current_state==M8)
-		begin
-			regop_finish <= 1'b1;
-		end
-		
-		if(sha_current_state==S1)
-		begin
-			//curr_sha_iter <= sha_iter_sum_wire;
-			wk_add_1 <= wk_add_sum_wire;
-			sig1_e_1 <= ({ah_regf[4][5:0],ah_regf[4][31:6]}^{ah_regf[4][10:0],ah_regf[4][31:11]}^{ah_regf[4][24:0],ah_regf[4][31:25]});
-			ch_efg_1 <= (ah_regf[4]&ah_regf[5])^((~ah_regf[4])&ah_regf[6]);
-			sig0_a_1 <= ({ah_regf[0][1:0],ah_regf[0][31:2]}^{ah_regf[0][12:0],ah_regf[0][31:13]}^{ah_regf[0][21:0],ah_regf[0][31:22]});
-			maj_abc_1 <= ((ah_regf[0]&ah_regf[1])^(ah_regf[0]&ah_regf[2])^(ah_regf[1]&ah_regf[2]));
-		end
-		else
-		if(sha_current_state==S2)
-		begin
-			wkh_add_2 <= wkh_add_2_sum_wire;
-			sig1ch_add_2 <= sig1ch_add_2_sum_wire;
-			T2_2 <= T2_2_sum_wire;
-		end
-		else
-		if(sha_current_state==S3)
-		begin
-			T1_3 <= T1_3_sum_wire;
-		end
-		else
-		if(sha_current_state==S4)
-		begin
-			curr_sha_iter <= sha_iter_sum_wire;
-			ah_regf[0] <= ah_regf0_sum_wire;
-			ah_regf[1] <= ah_regf[0];
-			ah_regf[2] <= ah_regf[1];
-			ah_regf[3] <= ah_regf[2];
-			ah_regf[4] <= ah_regf4_sum_wire;
-			ah_regf[5] <= ah_regf[4];
-			ah_regf[6] <= ah_regf[5];
-			ah_regf[7] <= ah_regf[6];
-		end
+		case(main_current_state)
+			M1:	begin
+					curr_addr_hop <= hop_addr_sum_wire;
+					regop_h_mem_en <= 1'b1;
+					ah_regf_wen_hold0 <= 1'b1;
+			end
+			
+			M3:	begin
+					curr_addr_kw <= kw_addr_sum_wire;
+					regop_w_mem_en <= 1'b1;
+					k_en_hold0 <= 1'b1;
+			end
+			
+			M7:	begin
+					curr_addr_kw <= kw_addr_sum_wire;
+					regop_w_mem_en <= 1'b1;
+					k_en_hold0 <= 1'b1;
+					
+					wk_add_1 <= wk_add_sum_wire;
+					sig1_e_1 <= ({ah_regf[4][5:0],ah_regf[4][31:6]}^{ah_regf[4][10:0],ah_regf[4][31:11]}^{ah_regf[4][24:0],ah_regf[4][31:25]});
+					ch_efg_1 <= (ah_regf[4]&ah_regf[5])^((~ah_regf[4])&ah_regf[6]);
+					sig0_a_1 <= ({ah_regf[0][1:0],ah_regf[0][31:2]}^{ah_regf[0][12:0],ah_regf[0][31:13]}^{ah_regf[0][21:0],ah_regf[0][31:22]});
+					maj_abc_1 <= ((ah_regf[0]&ah_regf[1])^(ah_regf[0]&ah_regf[2])^(ah_regf[1]&ah_regf[2]));
+			end
+			
+			M8:	begin
+					wkh_add_2 <= wkh_add_2_sum_wire;
+					sig1ch_add_2 <= sig1ch_add_2_sum_wire;
+					T2_2 <= T2_2_sum_wire;
+			end
+			
+			M9:	begin
+					T1_3 <= T1_3_sum_wire;
+			end
+			
+			M10:	begin
+						curr_sha_iter <= sha_iter_sum_wire;
+						ah_regf[0] <= ah_regf0_sum_wire;
+						ah_regf[1] <= ah_regf[0];
+						ah_regf[2] <= ah_regf[1];
+						ah_regf[3] <= ah_regf[2];
+						ah_regf[4] <= ah_regf4_sum_wire;
+						ah_regf[5] <= ah_regf[4];
+						ah_regf[6] <= ah_regf[5];
+						ah_regf[7] <= ah_regf[6];
+			end
+			
+			M11:	begin
+						curr_addr_hop <= hop_addr_sum_wire;
+						regop_h_mem_en <= 1'b1;
+						ah_regf_wen_hold0 <= 1'b1;
+			end
+			
+			M14:	begin
+						curr_addr_hop <= hop_addr_sum_wire;
+						regop_op_mem_en <= 1'b1;
+						regop_op_mem_wdata <= ah_regf[curr_addr_hop];
+			end
+			
+			default:	begin
+							ah_regf_wen_hold0 <= 1'b0;
+							regop_h_mem_en <= 1'b0;
+							regop_w_mem_en <= 1'b0;
+							k_en_hold0 <= 1'b0;
+							regop_op_mem_en <= 1'b0;
+			end
+		endcase
 	end
 end
 
 /** Main and SHA State Machine Procedural Block */
 always@(*)
 begin
-	/** Defaults To Avoid Latches */
-	go_iterate_sha = 1'b0;
-	ah_access_select = 1'b0;
-	main_state_ah_wen = 1'b0;
-	sha_state_ah_wen = 1'b0;
-	done_with_sha = 1'b0;
-	
+	/** Defaults to Avoid Latches */
+	finish_signal = 1'b0;
+	ah_access_sig = 1'b0;
+
 	case(main_current_state)
 		M0:	begin
 				if(regin_main_go_sig)
@@ -306,8 +283,7 @@ begin
 				end
 		end
 		
-		M1:	begin
-				main_state_ah_wen = 1'b1;
+		M1:	begin	//Copy a-h from H SRAM (2 Reg Address - Load First, 2 Reg WEN - Load First)
 				if(hop_addr_cout_wire)
 				begin
 					main_next_state = M2;
@@ -329,7 +305,7 @@ begin
 				end
 		end
 		
-		M3: begin
+		M3: begin	// +1 to KW Address and En -> 1
 				main_next_state = M4;
 		end
 		
@@ -342,120 +318,76 @@ begin
 		end
 		
 		M6:	begin
-				go_iterate_sha = 1'b1;
-				ah_access_select = 1'b1;
-				if(done_with_sha)
-				begin
-					main_next_state = M7;
-				end
-				else
-				begin
-					main_next_state = M6;
-				end
+			main_next_state = M7;
 		end
 		
-		M7:	begin
-				if(hop_addr_cout_wire)
-				begin
-					main_next_state = M8;
-				end
-				else
-				begin
-					main_next_state = M7;
-				end
+		M7:	begin	// +1 KW Address and En -> 1
+			main_next_state = M8;
 		end
 		
 		M8:	begin
-				if(regin_main_go_sig)
-				begin
-					main_next_state = M1;	//REset ALL HEre
-				end
-				else
-				begin
-					main_next_state = M8;
-				end
+				main_next_state = M9;
 		end
 		
-		default:	begin
-						main_next_state = M0;
-		end
-	endcase
-	
-	case(sha_current_state)
-		S0:	begin
-				done_with_sha = 1'b1;
-				if(go_iterate_sha)
-				begin
-					sha_next_state = S1;
-				end
-				else
-				begin
-					sha_next_state = S0;
-				end
+		M9:	begin
+				main_next_state = M10;
 		end
 		
-		S1:	begin
-				sha_next_state = S2;
-				/*
-				if(sha_iter_cout_wire)
-				begin
-					sha_next_state = S5;
-				end
-				else
-				begin
-					sha_next_state = S2;
-				end
-				*/
+		M10:	begin
+					ah_access_sig = 1'b1;
+					if(sha_iter_cout_wire)
+					begin
+						main_next_state = M11;
+					end
+					else
+					begin
+						main_next_state = M7;
+					end
 		end
 		
-		S2: begin
-				sha_next_state = S3;
+		M11:	begin	/** Addition H and AH */
+					ah_access_sig = 1'b1;
+					if(hop_addr_cout_wire)
+					begin
+						main_next_state = M12;
+					end
+					else
+					begin
+						main_next_state = M11;
+					end
 		end
 		
-		S3:	begin
-				sha_next_state = S4;
+		M12:	begin
+					ah_access_sig = 1'b1;
+					main_next_state = M13;
 		end
 		
-		S4:	begin
-				if(sha_iter_cout_wire)
-				begin
-					sha_next_state = S5;
-				end
-				else
-				begin
-					sha_next_state = S1;
-				end
-				//sha_next_state = S1;
+		M13:	begin
+					ah_access_sig = 1'b1;
+					main_next_state = M14;
 		end
 		
-		S5:	begin
-				sha_state_ah_wen = 1'b1;
-				if(hop_addr_cout_wire)
-				begin
-					sha_next_state = S6;
-				end
-				else
-				begin
-					sha_next_state = S5;
-				end
+		M14:	begin
+					if(hop_addr_cout_wire)
+					begin
+						main_next_state = M15;
+					end
+					else
+					begin
+						main_next_state = M14;
+					end
 		end
 		
-		S6:	begin
-				sha_next_state = S7;
-		end
-		
-		S7:	begin
-				done_with_sha = 1'b1;
-				/*
-				if(go_iterate_sha)
-				begin
-					sha_next_state = S1;
-				end
-				else
-				begin
-					sha_next_state = S7;
-				end
-				*/
+		M15:	begin
+					finish_signal = 1'b1;
+					if(regin_main_go_sig)
+					begin
+						main_next_state = M1;
+					end
+					else
+					begin
+						main_next_state = M15;
+					end
 		end
 	endcase
 end
@@ -473,3 +405,7 @@ DW01_add #(width32)	PPADD10 	(.A(T1_3), .B(T2_2), .CI(1'b0), .SUM(ah_regf0_sum_w
 DW01_add #(width32)	PPADD11 	(.A(T1_3), .B(ah_regf[3]), .CI(1'b0), .SUM(ah_regf4_sum_wire));
 
 endmodule
+
+/**
+vlog ../../../../HDL/run_s/DW01_add.v; vlog -sv ../../../../HDL/run_s/sram.v; vlog ../../../../HDL/run_s/gen_padded.v; vlog ../../../../HDL/run_s/gen_w_less.v; vlog ../../../../HDL/run_s/gen_h.v; vlog -sv ../../../../SIMULATION/run_s/gen_h_test.v;
+*/
